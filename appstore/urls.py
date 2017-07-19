e5b62667d51b9690e741e412ece3d01a0c90b8ef
Luke Hutton <admin@combustion-studios.com>
2017-07-19 10:10:06 +0100

    fix redirect at store root

--- appstore/urls.py
+++ appstore/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.views.generic import RedirectView
+
 
 urlpatterns = [
-    url(r'^store/', include("store.urls")),
+    url(r'^$', RedirectView.as_view(url='store/', permanent=True)),
+    url(r'^store/', include("store.urls"), name="store_redirect"),
     url(r'^admin/', admin.site.urls),
 ]
